21d84a5453adaeaa5e8d88e3ba5b3aa8790df16f
ccs100203 <ccs100203@gmail.com>
2018-12-24 03:31:42 +0800

    Add Change Scene(not finish)

---
+++ Assets/Scripts/SwitchScene/ExitGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class ExitGame : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData e)
+    {
+        Application.Quit();
+    }
+}
